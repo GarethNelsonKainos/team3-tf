37eebc967f3ffc7623c0cbcd5e73d72e7ee61460
tom-kainos <thomas.ferguson@kainos.com>
2026-03-04 16:19:00 +0000

    change to accesspolicy

--- main.tf
+++ main.tf
@@ -12,11 +12,6 @@ provider "azurerm" {
 }
 
 
-data "azurerm_role_definition" "kv_secrets_reader" {
-  name  = "Key Vault Secrets User"
-  scope = azurerm_key_vault.main.id
-}
-
 module "resource_group" {
   source              = "./modules/resource_groups"
   resource_group_name = var.resource_group_name
@@ -34,7 +29,6 @@ resource "azurerm_key_vault" "main" {
   soft_delete_retention_days    = var.key_vault_soft_delete_retention_days
   purge_protection_enabled      = var.key_vault_purge_protection_enabled
   public_network_access_enabled = false
-  enable_rbac_authorization    = true
 
   tags = merge(
     var.tags,
@@ -46,9 +40,10 @@ resource "azurerm_key_vault" "main" {
 
 data "azurerm_client_config" "current" {}
 
-resource "azurerm_role_assignment" "kv_reader_assignment" {
-  scope              = azurerm_key_vault.main.id
-  role_definition_id = data.azurerm_role_definition.kv_secrets_reader.id
-  principal_id       = var.key_vault_reader_group_object_id
-  depends_on         = [azurerm_key_vault.main]
+resource "azurerm_key_vault_access_policy" "kv_group_reader" {
+  key_vault_id = azurerm_key_vault.main.id
+  tenant_id    = data.azurerm_client_config.current.tenant_id
+  object_id    = var.key_vault_reader_group_object_id
+
+  secret_permissions = ["Get", "List"]
 }
